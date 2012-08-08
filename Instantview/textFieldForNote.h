165881968c6469c0bc06f4cad4f069ed96aef894
phoenixjah <phoenixjah@gmail.com>
2012-08-08 08:05:45 +0800

    add add options btns but no show

---
+++ Instantview/textFieldForNote.h
@@ -0,0 +1,13 @@
+//
+//  textFieldForNote.h
+//  Instantview
+//
+//  Created by Chia Lin on 12/8/4.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface textFieldForNote : UITextField
+
+@end
---
+++ Instantview/textFieldForNote.m
@@ -0,0 +1,31 @@
+//
+//  textFieldForNote.m
+//  Instantview
+//
+//  Created by Chia Lin on 12/8/4.
+//
+//
+
+#import "textFieldForNote.h"
+
+@implementation textFieldForNote
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
